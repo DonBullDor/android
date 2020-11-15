7a16281272a2da538ac160d4cfe7530c3493cec0
Mohamed Ben Abbes <mohamedbenabbes6@gmail.com>
2020-11-15 10:50:45 +0100

    updates

--- RuntimeFragments/app/src/main/java/com/example/runtimefragments/FragmentOne.java
+++ RuntimeFragments/app/src/main/java/com/example/runtimefragments/FragmentOne.java
@@ -1,4 +1,4 @@
-package com.example.createfragment;
+package com.example.runtimefragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
@@ -8,9 +8,10 @@ import android.view.ViewGroup;
 
 public class FragmentOne extends Fragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_one, container,
-                false);
+        return inflater.inflate(R.layout.fragment_one,
+                container, false);
     }
 }
---
+++ RuntimeFragments/app/src/main/java/com/example/runtimefragments/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.runtimefragments;
+
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    FragmentOne mFragmentOne;
+    FragmentTwo mFragmentTwo;
+    int showingFragment=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFragmentOne = new FragmentOne();
+        mFragmentTwo = new FragmentTwo();
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.add(R.id.frameLayout, mFragmentOne);
+//        fragmentTransaction.commit();
+        getFragmentManager().beginTransaction().add(R.id.frameLayout, mFragmentOne).commit();
+        showingFragment=1;
+    }
+
+    @SuppressLint("CommitTransaction")
+    public void switchFragment(View view) {
+        if (showingFragment==1) {
+            getFragmentManager().beginTransaction().replace(R.id.frameLayout, mFragmentTwo).commit();
+            showingFragment = 2;
+        } else {
+            getFragmentManager().beginTransaction().replace(R.id.frameLayout, mFragmentOne).commit();
+            showingFragment=1;
+        }
+
+    }
+}
